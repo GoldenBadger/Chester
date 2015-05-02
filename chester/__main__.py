f917221adedc70e6115a49e6671e19184f26b7b2
Terry Bolt <bolt.terry@gmail.com>
2015-05-02 16:32:40 +0100

    Added a __main__.py to run the IRC bot.

---
+++ chester/__main__.py
@@ -0,0 +1,22 @@
+"""Main module for Chester. Sets up an IRC bot according to the options
+specified in config.ini.
+"""
+
+from ConfigParser import ConfigParser
+
+from twisted.internet import reactor
+
+from chester.bot import ChessBotFactory
+
+if __name__ == "__main__":
+    config = ConfigParser()
+    config.read("config.ini")
+    host = config.get("irc", "host")
+    port = int(config.get("irc", "port"))
+    nickname = config.get("irc", "nickname")
+    realname = config.get("irc", "realname")
+    channel = config.get("irc", "channel")
+    
+    print "Read config file."
+    reactor.connectTCP(host, port, ChessBotFactory(channel, nickname, realname))
+    reactor.run()
